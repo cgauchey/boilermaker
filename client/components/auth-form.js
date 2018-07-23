de27e7796ef42862f7623ba2d060463f8e369a12
Celine Gauchey <celinegauchey@gmail.com>
2018-07-23 16:43:31 -0400

    github stuff

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -29,7 +29,8 @@ const AuthForm = props => {
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      {/* <a href="/auth/google">{displayName} with Google</a> */}
+      <a href="/auth/github">{displayName} with GitHub</a>
     </div>
   )
 }
